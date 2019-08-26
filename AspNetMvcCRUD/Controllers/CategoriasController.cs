a16fc6c64a25288ee61a57616fcc1c2c76652235
Fernando Tedokon <tedokon@gmail.com>
2019-08-26 11:21:46 -0300

    Initial commit

---
+++ AspNetMvcCRUD/Controllers/CategoriasController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using AspNetMvcCRUD.Models;
+
+namespace AspNetMvcCRUD.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private LojaContext db = new LojaContext();
+
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            return View(db.Categoria.ToList());
+        }
+
+        // GET: Categorias/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categoria categoria = db.Categoria.Find(id);
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
+        }
+
+        // GET: Categorias/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categorias/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome")] Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categoria.Add(categoria);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(categoria);
+        }
+
+        // GET: Categorias/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categoria categoria = db.Categoria.Find(id);
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
+        }
+
+        // POST: Categorias/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome")] Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(categoria).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(categoria);
+        }
+
+        // GET: Categorias/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categoria categoria = db.Categoria.Find(id);
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
+        }
+
+        // POST: Categorias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            if (id <= 0)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+
+            Categoria categoria = db.Categoria.Find(id);
+
+            if (categoria == null)
+                return HttpNotFound();
+
+            //if (categoria.Produto.Count(v => v.Ativo) > 0)
+            //return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Essa categoria não pode ser excluída, pois há produtos ativas relacionadas a ela.");
+            
+            db.Categoria.Remove(categoria);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AspNetMvcCRUD/Controllers/ProdutosController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AspNetMvcCRUD.Models;
+
+namespace AspNetMvcCRUD.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private LojaContext db = new LojaContext();
+
+        // GET: Produtos
+        public ActionResult Index(string busca = null)
+        {
+            if (busca != null)
+                return View(db.Produto.Where(a => a.Nome.ToUpper().Contains(busca.ToUpper())).ToList());
+            return View(db.Produto.ToList());
+        }
+
+        // GET: Produtos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = db.Produto
+                                .Include(c => c.Categoria)
+                                .FirstOrDefault(c => c.Id == id);
+
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        // GET: Produtos/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdCategoria = new SelectList(db.Categoria, "Id", "Nome");
+            return View();
+        }
+
+        // POST: Produtos/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome,Preco,DataCadastro,IdCategoria,Ativo")] Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Produto.Add(produto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdCategoria = new SelectList(db.Categoria, "Id", "Nome", produto.IdCategoria);
+            return View(produto);
+        }
+
+        // GET: Produtos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = db.Produto.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdCategoria = new SelectList(db.Categoria, "Id", "Nome", produto.IdCategoria);
+            return View(produto);
+        }
+
+        // POST: Produtos/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome,Preco,DataCadastro,IdCategoria,Ativo")] Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(produto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdCategoria = new SelectList(db.Categoria, "Id", "Nome", produto.IdCategoria);
+            return View(produto);
+        }
+
+        // GET: Produtos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = db.Produto.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        // POST: Produtos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Produto produto = db.Produto.Find(id);
+            db.Produto.Remove(produto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
